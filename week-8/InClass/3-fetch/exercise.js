035bb64a8cc393fd6052496a56102eef08b4c061
mara-ber <mara.berezin@gmail.com>
2020-07-04 14:02:39 +0100

    week8 inClass 3,4

--- week-8/InClass/3-fetch/exercise.js
+++ week-8/InClass/3-fetch/exercise.js
@@ -5,3 +5,17 @@
     Remember to open "index.html" using Live Preview, and test that you get the correct results in the console!
     (you should see "Alex")
  */
+
+function aboutPer(person) {
+  return `${person.name} is ${person.age}`;
+}
+
+fetch("data.json")
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (persons) {
+    persons.forEach((person) => console.log(aboutPer(person)));
+    //console.log(`${person.name} is ${person.age}`));
+    //console.log(persons[0].name)     // ---> second part of the exercise
+  });
--- week-8/InClass/4-APIs/exercise.js
+++ week-8/InClass/4-APIs/exercise.js
@@ -7,3 +7,19 @@
 
        Remember to open "index.html" using Live Preview, and test that you get the correct results in the console!
  */
+
+fetch("https://cat-fact.herokuapp.com/facts")
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (cats) {
+    console.log(cats.all[0].text);
+  });
+
+fetch("https://restcountries.eu/rest/v2/name/Great%20Britain?fullText=true")
+  .then(function (results) {
+    return results.json();
+  })
+  .then(function (UK) {
+     console.log(UK[0].population);
+  });
